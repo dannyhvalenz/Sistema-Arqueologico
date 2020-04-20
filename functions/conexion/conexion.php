94a9186e54f1cd466708c05625ff1bda4f6d06b0
Daniela H Valenzuela <dannyhvalenz@gmail.com>
2020-04-19 21:30:02 -0500

    Actualizacion SQL

--- functions/conexion/conexion.php
+++ functions/conexion/conexion.php
@@ -1,6 +1,12 @@
 <?php
     // Crear conexion
-    $conexion = mysqli_connect("localhost", "root", "12345", "abdarqueologia");
+    $host = 'localhost';
+    $user = 'root';
+    $password = '12345';
+    $bd = 'abdarqueologia';
+
+    $conexion = mysqli_connect($host, $user, $password, $bd);
+    //$conexion = mysqli_connect("localhost", "root", "12345", "abdarqueologia");
 
     // Checar conexion
     if ($conexion->connect_error) {
