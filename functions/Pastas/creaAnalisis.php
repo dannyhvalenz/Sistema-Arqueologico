a4a9dfa2b8e6758ae59128ad7c53933ce8e5bf24
Leonardo <leonardogalicia2412@gmail.com>
2020-05-03 20:12:20 -0500

    actualizacion

--- functions/Pastas/creaAnalisis.php
+++ functions/Pastas/creaAnalisis.php
@@ -20,8 +20,7 @@
     $Observaciones = $_POST['Observaciones'];
     $Estado = 'activo';
     
-    $ds = DIRECTORY_SEPARATOR;  //variable para el separador de directorio
-
+    
     $sql = "INSERT INTO analisispastas (NumAnalisisPasta, Fecha, Cuadrante, Sitio, Patron, 
                 Utme, Utmn, Latitud, Analizo, Bolsa, Tipo, Tratamiento, Modificacion, FormasTratamientos, 
                 Peso, TotalFragmentos, Observaciones, Estado) VALUES ('$NumAnalisisPasta', '$Fecha', '$Cuadrante', 
@@ -33,76 +32,27 @@
 //mysqli_insert_id($conn);
     //$ruta = 'files/'.$id_insert.'/';
     
-	$id_insert = mysqli_insert_id($conexion);
+	//$id_insert = mysqli_insert_id($conexion);
     $resultado=mysqli_query($conexion, $sql);
-	$storeFolder = 'uploads/'.$id_insert.'/';   //carpeta donde se alojara 
-	mkdir($storeFolder);
-    if (!empty($_FILES)) {
-
-            $tempFile = $_FILES['file']['tmp_name'];          //Si el archivo se envía a la página, almacene el objeto del archivo en una variable temporal.             
-
-            $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //Cree la ruta absoluta de la carpeta de destino.
-
-            $targetFile =  $targetPath. $_FILES['file']['name'];  //Cree la ruta absoluta del destino del archivo cargado.
-
-            move_uploaded_file($tempFile,$targetFile); //Mover el archivo cargado al destino.
-
-        }
-	/*$id_insert = mysqli_insert_id($conexion);
 	
-	if($_FILES["archivo"]["error"]>0){
-		echo "Error al cargar archivo";	
-		} else {
-		
-		$permitidos = array("image/gif","image/png","application/pdf");
-		$limite_kb = 200;
-		
-		if(in_array($_FILES["archivo"]["type"], $permitidos) && $_FILES["archivo"]["size"] <= $limite_kb * 1024){
-			
-			$ruta = 'files/'.$id_insert.'/';
-			$archivo = $ruta.$_FILES["archivo"]["name"];
-			
-			if(!file_exists($ruta)){
-				mkdir($ruta);
-			}
-			
-			if(!file_exists($archivo)){
-				
-				$resultado = @move_uploaded_file($_FILES["archivo"]["tmp_name"], $archivo);
-				
-				if($resultado){
-					echo "Archivo Guardado";
-					} else {
-					echo "Error al guardar archivo";
-				}
-				
-				} else {
-				echo "Archivo ya existe";
-			}
-			
-			} else {
-			echo "Archivo no permitido o excede el tamaño";
-		}
-		
-	}*/
-	
-	/*if(!file_exists($storeFolder)){
-				mkdir($storeFolder);
+	//$storeFolder = 'uploads/'.$id_insert.'/';   //carpeta donde se alojara 
+	mkdir($storeFolder);//crear el folder
+    
+	//agregar imagenes
+	$ds = DIRECTORY_SEPARATOR;  //variable para el separador de directorio
+ 
+	$storeFolder = 'imagenes';   //Declarar una variable para la carpeta de destino. 
+
+	if (!empty($_FILES)) {
+
+		$tempFile = $_FILES['file']['tmp_name'];//Si el archivo se envía a la página, almacena el objeto del archivo en una variable temporal.             
+
+		$targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //Crea la ruta absoluta de la carpeta de destino.
+
+		$targetFile =  $targetPath. $_FILES['file']['name'];  //Crea la ruta absoluta del destino del archivo cargado.
+		move_uploaded_file($tempFile,$targetFile); //Mover el archivo cargado al destino.
+
 	}
-	$id_insert = mysqli_insert_id();
-	$storeFolder = 'uploads/'.$id_insert.'/';   //carpeta donde se alojara 
-	mkdir($storeFolder);
-    if (!empty($_FILES)) {
-
-            $tempFile = $_FILES['file']['tmp_name'];          //Si el archivo se envía a la página, almacene el objeto del archivo en una variable temporal.             
-
-            $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //Cree la ruta absoluta de la carpeta de destino.
-
-            $targetFile =  $targetPath. $_FILES['file']['name'];  //Cree la ruta absoluta del destino del archivo cargado.
-
-            move_uploaded_file($tempFile,$targetFile); //Mover el archivo cargado al destino.
-
-        }/**/ 
 
     if($resultado==true){    
         
